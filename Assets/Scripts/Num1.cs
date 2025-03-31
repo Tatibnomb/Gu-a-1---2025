c5707a3d56cf3548bb63069d583871f28d8045f1
Tatibnomb <49379010@est.ort.edu.ar>
2025-03-31 10:43:12 -0300

    Guía 2 ej 1 terminado

---
+++ Assets/Scripts/Num1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Num1 : MonoBehaviour
+{
+    public int num1;
+    int resultado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        num1 = 0;
+        resultado = num1 + 2;
+        Debug.Log(resultado);
+
+        resultado = resultado * resultado;
+        Debug.Log(resultado);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

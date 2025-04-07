073b25e1716f85a740ceb8d0e80534e311feb93d
Tatibnomb <49379010@est.ort.edu.ar>
2025-04-07 10:41:40 -0300

    tema 3 puntos 1. y 2. terminados

--- Assets/Scripts/ComidaDeAnimalitos.cs
+++ Assets/Scripts/ComidaDeAnimalitos.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class ComidaDeAnimalitos : MonoBehaviour
 {
+    public string codigo;
+    public int diasDeEstadia;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       if(codigo == "G" && diasDeEstadia > 3)
+        {
+
+        }else if (codigo == "PP" && diasDeEstadia > 3)
+        {
+
+        }else if (codigo == "PG" && diasDeEstadia > 3)
+        {
+
+        }else
+        {
+            Debug.Log("El código y/o días de estadía no son válidos");
+            return;
+        }
     }
 
     // Update is called once per frame
